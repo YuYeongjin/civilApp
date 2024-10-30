cfb163931b6d3788de85e64433c6c8a1d6b74d88
YuYeongjin <108460833+YuYeongjin@users.noreply.github.com>
2024-10-30 20:01:56 +0900

    first init

---
+++ src/main/java/app/civil/civilapp/CivilAppApplication.java
@@ -0,0 +1,13 @@
+package app.civil.civilapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CivilAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CivilAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/civil/civilapp/CivilAppApplicationTests.java
@@ -0,0 +1,13 @@
+package app.civil.civilapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CivilAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
