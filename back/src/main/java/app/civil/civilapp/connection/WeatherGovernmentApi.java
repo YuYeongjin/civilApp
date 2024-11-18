72361986ff976686904a72074da73105bd1b1b39
YuYeongjin <108460833+YuYeongjin@users.noreply.github.com>
2024-11-18 20:17:29 +0900

    weather api dto converting test

--- back/src/main/java/app/civil/civilapp/connection/WeatherGovernmentApi.java
+++ back/src/main/java/app/civil/civilapp/connection/WeatherGovernmentApi.java
@@ -1,21 +1,39 @@
 package app.civil.civilapp.connection;
 
+import app.civil.civilapp.dto.weather.WeatherContents;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import java.nio.charset.Charset;
+
 @Slf4j
+@RequiredArgsConstructor
 public class WeatherGovernmentApi {
+    private final ObjectMapper objectMapper;
+
 
     // WebUI Login
 
-    public static Object getBlockWithAPI(String primaryServerUri, String api, String path, Class<?> clazz) {
-        return WeatherWebClient.createWithAPI(primaryServerUri, api)
+    public Object getBlockWithAPI(String primaryServerUri, String api, String path, Class<?> clazz) {
+try{
+
+
+        String res = WeatherWebClient.createWithAPI(primaryServerUri, api)
                 .get()
                 .uri(path)
                 .retrieve()
-                .bodyToMono(clazz)
+                .bodyToMono(String.class)
+                .map(body -> new String(body.getBytes(Charset.forName("ISO-8859-1")), Charset.forName("EUC-KR"))) // Decode EUC-KR
+
                 .block()
                 ;
-    }
+        return objectMapper.readValue(res, WeatherContents.class);
+}catch (Exception e){
+    e.printStackTrace();
+    return null;
+}
+     }
 
 
 }
--- back/src/main/java/app/civil/civilapp/util/WeatherAPI.java
+++ back/src/main/java/app/civil/civilapp/util/WeatherAPI.java
@@ -1,5 +1,7 @@
 package app.civil.civilapp.util;
 
+import app.civil.civilapp.connection.WeatherGovernmentApi;
+import app.civil.civilapp.connection.WeatherWebClient;
 import app.civil.civilapp.dto.weather.WeatherContents;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -9,14 +11,24 @@ import org.springframework.web.client.RestTemplate;
 public class WeatherAPI {
 
     public static String getGroundInfo(String tm, String stn, String help,String authKey ) {
+   //     String path =  "api/typ01/url/kma_sfctm2.php?tm="+tm+"&stn="+stn+"&help="+help+"&authKey="+authKey;
         String path = "https://apihub.kma.go.kr/api/typ01/url/kma_sfctm2.php?tm="+tm+"&stn="+stn+"&help="+help+"&authKey="+authKey;
         System.out.println("PATH : "+path);
 
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<String> response = restTemplate.getForEntity(path, String.class);
 
-        WeatherContents weather = restTemplate.getForObject(path, WeatherContents.class);
+       // WeatherContents weather = restTemplate.getForObject(path, WeatherContents.class);
+        /*
+        WeatherContents weather = (WeatherContents) WeatherGovernmentApi.getBlockWithAPI("https://apihub.kma.go.kr/","https://apihub.kma.go.kr/"+path,path,WeatherContents.class);
+        System.out.println(WeatherGovernmentApi.getBlockWithAPI("https://apihub.kma.go.kr/","https://apihub.kma.go.kr/"+path,path,String.class));
+       System.out.println("@@@@@@@@@@@@@@@@@@@2");
         System.out.println(weather);
+
+         */
+
         return response.getBody();
     }
+
 }
+
